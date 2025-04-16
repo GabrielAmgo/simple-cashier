4b901193b32127e6122ad5eec2066d59fdb255ab
gabrielamgo <gabrielp.amigo@gmail.com>
2025-04-16 14:56:03 -0300

    first commit

---
+++ src/main/java/com/example/simplecashier/SimpleCashierApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplecashier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCashierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCashierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simplecashier/SimpleCashierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplecashier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCashierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
